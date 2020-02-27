82a588ab7dd7e997524c87beb3cf2d391357b7e9
Travis Neuman <travis@neuman.dev>
2020-02-27 17:40:11 -0500

    upload

--- homepage/ViewController.swift
+++ homepage/ViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import WebKit
+class ViewController: UIViewController, WKUIDelegate {
+    var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            
+        let myURL = URL(string:"https://www.thedeadrobot.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
-
-
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
 }
 
